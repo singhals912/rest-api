4a4ef46681ec52bf64aca37f3b1448daf238de53
Esha Kaushal <84419207+EshaKaushal@users.noreply.github.com>
2021-05-27 09:05:08 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -217,4 +217,4 @@ def main():
 if __name__ == '__main__':
     # Run our Flask app and start listening for requests
     main()
-    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
