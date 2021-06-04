b83c1ea272708d9811150ba7bba55fa57b8e16ed
Esha Kaushal <84419207+EshaKaushal@users.noreply.github.com>
2021-06-03 20:43:04 -0400

    Update restapi_test.py

--- restapi_test.py
+++ restapi_test.py
@@ -1,5 +1,9 @@
 import pytest
 from Assignment2 import create_app
+import time
+import sqlite3
+
+timestamp = int(time.time())
 
 @pytest.fixture
 def client():
@@ -10,4 +14,26 @@ def client():
 
 def test_health(client):
     r = client.get("/")
+    assert 200 == r.status_code
+
+def test_addModel(client):
+    print("Testing Add Model functionality")
+    # Test add a model functionality
+    payload = {
+        "name": "bert-tiny",
+        "tokenizer": "mrm8488/bert-tiny-5-finetuned-squadv2",
+        "model": "mrm8488/bert-tiny-5-finetuned-squadv2"
+    }
+    r = client.put("/models", json=payload)
+    assert 200 == r.status_code
+
+
+def test_getModel(client):
+    # test /models GET
+    r = client.get("/models")
+    assert 200 == r.status_code
+
+def test_deleteModel(client):
+    # Test /models DELETE
+    r = client.delete("/models?model=bert-tiny")
     assert 200 == r.status_code
