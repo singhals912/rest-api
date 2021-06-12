27e3b45f2e1f54a562be9497e7f1d1a4e505cafd
Esha Kaushal <84419207+EshaKaushal@users.noreply.github.com>
2021-06-11 23:00:24 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -13,7 +13,7 @@ if not os.path.exists('.ssl'):
 
 filecontents = os.environ.get('GCS_CREDS')
 decoded_creds = filecontents.replace('@','=')
-decoded_creds = pybase64.b64decode(filecontents.decode('utf-8'))
+decoded_creds = pybase64.b64decode(filecontents)
 with open('/app/creds.json', 'w') as f:
     f.write(decoded_creds)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/app/creds.json'
